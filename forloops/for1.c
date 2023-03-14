b05da4a7b34e3d990a650539d28ce84597d4f051
Alok Chedambath <alokchedambath1@gmail.com>
2023-03-14 21:21:18 +0530

    Create for1.c

---
+++ forloops/for1.c
@@ -0,0 +1,13 @@
+//square pattern w/o nested for loops
+#include<stdio.h>
+
+int main()
+{
+    //statement 1 = intializing variable i
+    //statement 2 = checking condition 
+    //statement 3 = increasing the value of i
+    for (int i = 0; i < 4; i++)
+    {
+        printf("####\n");
+    }
+}
