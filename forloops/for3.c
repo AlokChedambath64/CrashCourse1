6b3515261df9958a3b58fe0133e98ca05781a67a
Alok Chedambath <alokchedambath1@gmail.com>
2023-03-14 21:22:27 +0530

    Create for3.c

---
+++ forloops/for3.c
@@ -0,0 +1,14 @@
+//Continue statement example
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 10; i++)
+    {
+        if (i == 7)
+        {
+            continue;
+        }
+        printf("%d\n", i);
+    }
+}
