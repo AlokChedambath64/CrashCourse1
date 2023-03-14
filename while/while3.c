a85b03bc3aa2c8a8a0387d2aa73bb696b8233862
Alok Chedambath <alokchedambath1@gmail.com>
2023-03-14 21:18:19 +0530

    Create while3.c

---
+++ while/while3.c
@@ -0,0 +1,18 @@
+//printing numbers form 1 to n
+#include <stdio.h>
+
+int main()
+{
+    int n;
+
+    printf("How many numbers: ");
+    scanf("%d", &n);
+
+    int i;
+
+    while(i < n)
+    {
+        printf("%d\n", i);
+        i++;
+    }
+}
