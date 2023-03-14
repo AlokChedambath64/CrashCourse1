920f3b61f200444c3a939c5861501ac7429f16bf
Alok Chedambath <alokchedambath1@gmail.com>
2023-03-14 21:17:45 +0530

    Create while2.c

---
+++ while/while2.c
@@ -0,0 +1,14 @@
+//Printing Numbers from 1 to 10
+#include <stdio.h>
+
+int main()
+{
+    int i = 0;
+
+    //while loop with condition (i <= 10)
+    while (i <= 10)
+    {
+        //printing the numbers
+        printf("%d\n", i);
+    }
+}
