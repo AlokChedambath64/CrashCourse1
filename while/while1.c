e2e4bef6f41b74d6be81473802737042d62ba82a
Alok Chedambath <alokchedambath1@gmail.com>
2023-03-14 21:13:59 +0530

    Create while1.c

---
+++ while/while1.c
@@ -0,0 +1,15 @@
+//While loop basic model, 0 to 4 
+#include <stdio.h>
+
+int main()
+{
+    //intializing variable i
+    int i =0;
+    //while loop with condition (i < 5)
+    while(i < 5){
+        //printing the value of i
+        printf("%d\n", i);
+        //incrementing the value of i
+        i++;
+    }
+}
