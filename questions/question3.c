8f47003257cb6f07e3b2b09b30abe9df9e120369
Alok Chedambath <alokchedambath1@gmail.com>
2023-03-14 21:32:59 +0530

    Create question3.c

---
+++ questions/question3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    char ch = 'A';
+    for (int i = 0; i < 6; i++)
+    {
+        for(int j =0; j <i; j++)
+        {
+            printf("%c", ch);
+            ch++;
+        } 
+        printf("\n");
+    }
+}
