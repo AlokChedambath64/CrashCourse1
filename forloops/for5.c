806b3064490457a0240430f6f27e21194a044caa
Alok Chedambath <alokchedambath1@gmail.com>
2023-03-14 21:23:39 +0530

    Create for5.c

---
+++ forloops/for5.c
@@ -0,0 +1,15 @@
+//understanding for loops
+//box using nested for loops
+#include <stdio.h>
+
+int main()
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 1; j++)
+        {
+            printf("#\n");
+        }
+        printf("\n");
+    }
+}
